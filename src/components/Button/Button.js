075e84dd1f7553dc319e359db16c9d9f1af86132
cart274@gmail.com <cart274@gmail.com>
2019-07-21 20:23:33 -0300

    Validación de formulario

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -5,7 +5,10 @@ import style from "./Button.module.css";
 const Button = (props)=> {
 
   return (
-    <button className={`${style.button} ${style[props.btnType]}`}>
+    <button 
+      type="button" 
+      className={`${style.button} ${props.disabled ? style.disabled : style[props.btnType] }`}
+      onClick={ props.onClick }>
       {props.text}
     </button>
   );
--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -6,33 +6,119 @@ import style from './Login.module.css'
 class Login extends Component {
 
   constructor(props){
-    super(props)
+    super(props);
+
+    this.state = {
+      name: '',
+      lastname: '',
+      email: '',
+      age: '',
+      ageList: [],
+      terms: false,
+      isValid: false
+    }
   }
+
+  componentDidMount(){
+    let ageList = [{text: 'Seleccione', value: 0}];
+
+    for(let year= 18; year<= 100; year++){
+      ageList.push({text: year, value: year})
+    }
+
+    this.setState({ageList});
+  }
+
+  changeName(e){
+    let { value = ''} = e.target;
+    this.setState({name: value}, () => {
+      this.validate();
+    })    
+  } 
+
+  changeLastname(e){
+    let { value = '' } = e.target;
+    this.setState({lastname: value}, () => {
+      this.validate();
+    })  
+  } 
+
+  changeEmail(e){
+    let { value = '' } = e.target;
+    this.setState({email: value}, () => {
+      this.validate();
+    })  
+  } 
+
+  changeAge(e){
+    let { value } = e.target;
+    this.setState({age: value}, () => {
+      this.validate();
+    })  
+  } 
   
+  changeTerms(e){
+    let { value } = e.target;
+    this.setState({terms: value}, () => {
+      this.validate();
+    })  
+  } 
+
+  validate(e){
+    let {name, lastname, email, age, isValid, terms} = this.state,
+    validated = false;
+
+    if(name && lastname && email && age && terms && this.validateEmail(email) ){
+      if(email)
+        validated = true;
+    }
+
+    if(isValid !== validated)
+      this.setState({isValid: validated});
+  }
+
+  validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  }
+
+  send(e){
+    e.preventDefault();
+    console.log("send");
+  }
+
   render() {
+    let {name, lastname, email, age, ageList, terms, isValid} = this.state;
+
     return (
       <section className={style.login}>
         <h1>Login</h1>
         <form>
           <div className={style.leftLogin}>
             <label for="name">Nombre</label>
-            <input type="text" placeholder="Nombre" id="name"/>
+            <input type="text" placeholder="Nombre" id="name" value={name} onChange={(e)=> this.changeName(e)}/>
             <label for="email">Email</label>
-            <input type="mail" placeholder="Email" id="email"/>
+            <input type="mail" placeholder="Email" id="email" value={email} onChange={(e)=> this.changeEmail(e)}/>
           </div>
           <div className={style.rightLogin}>
             <label for="lastname">Apellido</label>
-            <input type="text"  placeholder="Apellido" id="lastname"/>
+            <input type="text"  placeholder="Apellido" id="lastname" value={lastname} onChange={(e)=> this.changeLastname(e)}/>
             <label for="age">Edad</label>
-            <select id="age"></select>
+            <select id="age" onChange={(e)=> this.changeAge(e)} value={age}>
+              {
+                Array.isArray(ageList) && ageList.map( ({value = 0, text = ''}, key) =>{
+                  return <option key={ key } value={value} >{text}</option>
+                })
+              }
+            </select>
           </div>
           <div className={style.termsLogin}>
-            <input type="checkbox" id="terms"></input> 
+            <input type="checkbox" id="terms" defaultChecked={terms} onChange={(e) =>this.changeTerms(e)}></input> 
             <label for="terms">Acepto los términos y condiciones</label>
           </div>
           <div className={style.btnGroup}>
             <Button text="Cancelar" btnType="tertiary"></Button>
-            <Button text="Entrar" btnType="primary"></Button>
+            <Button text="Entrar" btnType="primary" disabled={!isValid} onClick={(e)=> this.send(e)}></Button>
           </div>
         </form>
       </section>
